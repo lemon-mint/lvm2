9b4ac12f45327c027d767fd9a985372f5d55c7fc
lemon-mint <ice.lemons.mint@gmail.com>
2022-04-08 18:15:52 +0900

    HALF_WORD_SIZE, BYTE_SIZE

--- isa.go
+++ isa.go
@@ -2,7 +2,9 @@ package lvm2
 
 type InstructionType byte
 
-const WORD_SIZE = 8
+const WORD_SIZE = 4
+const HALF_WORD_SIZE = WORD_SIZE / 2
+const BYTE_SIZE = 1
 
 const (
 	InstructionType_NOP InstructionType = iota
@@ -30,12 +32,20 @@ const (
 	InstructionType_JGE // if R0 >= 0; PC = R1
 	InstructionType_JLE // if R0 <= 0; PC = R1
 
-	InstructionType_PUSH // SP = SP - WORD_SIZE
-	InstructionType_POP  // SP = SP + WORD_SIZE
+	InstructionType_LOAD  // R0 = [MEM[R1 + R2]] (Load Register from Memory (WORD_SIZE))
+	InstructionType_LOADH // R0 = [MEM[R1 + R2]] (Load Register from Memory (HALF_WORD_SIZE))
+	InstructionType_LOADB // R0 = [MEM[R1 + R2]] (Load Register from Memory (BYTE_SIZE))
 
-	InstructionType_LOAD  // R0 = [MEM[R1 + R2]]
-	InstructionType_STORE // [MEM[R1 + R2]] = R0
-	InstructionType_MOV   // R0 = R1
+	InstructionType_STORE  // [MEM[R1 + R2]] = R0 (Store Register to Memory (WORD_SIZE))
+	InstructionType_STOREH // [MEM[R1 + R2]] = R0 (Store Register to Memory (HALF_WORD_SIZE))
+	InstructionType_STOREB // [MEM[R1 + R2]] = R0 (Store Register to Memory (BYTE_SIZE))
+
+	InstructionType_MOV  // R0 = R1 (Move Register by WORD_SIZE)
+	InstructionType_MOVH // R0 = R1 (Move Register by HALF_WORD_SIZE)
+	InstructionType_MOVB // R0 = R1 (Move Register by BYTE_SIZE)
+
+	InstructionType_PUSH // SP = SP - WORD_SIZE (stack.push(R0))
+	InstructionType_POP  // SP = SP + WORD_SIZE (R0 = stack.pop())
 
 	InstructionType_CALL // SP = SP - WORD_SIZE; [SP] = PC; PC = R0
 	InstructionType_RET  // PC = [SP]; SP = SP + WORD_SIZE
