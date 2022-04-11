7e243284f134402cf51799ef0379657c49cd377f
lemon-mint <ice.lemons.mint@gmail.com>
2022-04-11 16:58:27 +0900

    Registers

--- vm.go
+++ vm.go
@@ -3,7 +3,22 @@ package lvm2
 type VM struct {
 	Memory *Memory
 
-	Registers [64 + 32]uint64
+	// # Registers
+	//
+	// ## 64-bit General Purpose Registers
+	//
+	// R0-R31
+	//
+	// ## SYSCALL registers
+	//
+	// SYS32-SYS63
+	//
+	// ## VM registers
+	//
+	// Program Counter (PC) (Register ID: 64)
+	// Stack Pointer (SP)   (Register ID: 65)
+	// Stack Base (SB)      (Register ID: 66)
+	Registers [32 + 32 + 3]uint64
 }
 
 const Op0TypeMask = 0b11000000
