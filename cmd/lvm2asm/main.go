102b8ef63b7a2ff7f130c3e299ee10249a81e7f3
lemon-mint <ice.lemons.mint@gmail.com>
2022-04-28 21:48:09 +0900

    lvm2asm

--- cmd/lvm2asm/main.go
+++ cmd/lvm2asm/main.go
@@ -1,238 +1,212 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
-type TokenType string
-
-const (
-	TokenType_EOF        = "EOF"
-	TokenType_NUMBER     = "NUMBER"
-	TokenType_STRING     = "STRING"
-	TokenType_IDENTIFIER = "IDENTIFIER"
-	TokenType_REGISTER   = "REGISTER"
-	TokenType_ADD        = "ADD"
-	TokenType_SUB        = "SUB"
-	TokenType_COMMA      = "COMMA"
-	TokenType_COMMENT    = "COMMENT"
-
-	TokenType_LBRACKET = "LBRACKET"
-	TokenType_RBRACKET = "RBRACKET"
-
-	TokenType_INSTRUCTION = "INSTRUCTION"
-	TokenType_COMPILER    = "COMPILER"
+	"github.com/alecthomas/participle/v2"
+	"github.com/lemon-mint/lvm2"
+	"github.com/lemon-mint/lvm2/asm"
 )
 
-type Token struct {
-	Type  TokenType
-	Value string
-
-	Line int
-	Col  int
-
-	Pos int
+type Instruction struct {
+	Name     string     `parser:"@Ident"`
+	Operands []*Operand `parser:"(@@ ','?)*"`
 }
 
-func NewToken(type_ TokenType, value_ string, line_ int, col_ int, pos_ int) *Token {
-	return &Token{
-		Type:  type_,
-		Value: value_,
-
-		Line: line_,
-		Col:  col_,
-
-		Pos: pos_,
-	}
+type Register struct {
+	Name string `parser:"\"%\" @Ident"`
 }
 
-type Lexer struct {
-	data []rune
-	pos  int
+type Sign bool
 
-	line int
-	col  int
-
-	state map[string]interface{}
-
-	char      rune
-	isEscaped bool
-
-	lastToken *Token
-
-	tokens []*Token
-
-	err error
+func (b *Sign) Capture(values []string) error {
+	*b = values[0] == "-"
+	return nil
 }
 
-func (l *Lexer) ReadChar() bool {
-	if l.pos >= len(l.data) {
-		l.char = 0
-		return false
-	}
-
-	l.char = l.data[l.pos]
-	l.pos++
-
-	if l.char == '\n' {
-		l.line++
-		l.col = 0
-	} else {
-		l.col++
-	}
-
-	return true
+type Operand struct {
+	Register *Register `parser:"  @@ |"`
+	Sign     Sign      `parser:" @('-' | '+')?"`
+	Int      *int64    `parser:" @Int"`
+	String   *string   `parser:"| @String"`
+	Variable *string   `parser:"| \"@\"@Ident"`
 }
 
-func (l *Lexer) SkipWhitespace() bool {
-	for l.char != 0 && (l.char == ' ' || l.char == '\t' || l.char == '\n' || l.char == '\r') {
-		if !l.ReadChar() {
-			return false
-		}
-	}
-	return true
-}
-
-var asm_keywords = map[string]bool{
-	"NOP":     true,
-	"ADD":     true,
-	"SUB":     true,
-	"MUL":     true,
-	"DIV":     true,
-	"MOD":     true,
-	"AND":     true,
-	"OR":      true,
-	"XOR":     true,
-	"NOT":     true,
-	"SHL":     true,
-	"SHR":     true,
-	"CMP":     true,
-	"JMP":     true,
-	"JG":      true,
-	"JL":      true,
-	"JE":      true,
-	"JNE":     true,
-	"JGE":     true,
-	"JLE":     true,
-	"LOAD":    true,
-	"LOADH":   true,
-	"LOADB":   true,
-	"STORE":   true,
-	"STOREH":  true,
-	"STOREB":  true,
-	"MOV":     true,
-	"MOVH":    true,
-	"MOVB":    true,
-	"PUSH":    true,
-	"POP":     true,
-	"CALL":    true,
-	"RET":     true,
-	"SYSCALL": true,
-}
-
-var compiler_keywords = map[string]bool{
-	"SECTION": true,
-	"ALIGN":   true,
-	"CHARS":   true,
-	"LABEL":   true,
-}
-
-var register_keywords = map[string]bool{
-	"$R0":    true,
-	"$R1":    true,
-	"$R2":    true,
-	"$R3":    true,
-	"$R4":    true,
-	"$R5":    true,
-	"$R6":    true,
-	"$R7":    true,
-	"$R8":    true,
-	"$R9":    true,
-	"$R10":   true,
-	"$R11":   true,
-	"$R12":   true,
-	"$R13":   true,
-	"$R14":   true,
-	"$R15":   true,
-	"$R16":   true,
-	"$R17":   true,
-	"$R18":   true,
-	"$R19":   true,
-	"$R20":   true,
-	"$R21":   true,
-	"$R22":   true,
-	"$R23":   true,
-	"$R24":   true,
-	"$R25":   true,
-	"$R26":   true,
-	"$R27":   true,
-	"$R28":   true,
-	"$R29":   true,
-	"$R30":   true,
-	"$R31":   true,
-	"$SYS32": true,
-	"$SYS33": true,
-	"$SYS34": true,
-	"$SYS35": true,
-	"$SYS36": true,
-	"$SYS37": true,
-	"$SYS38": true,
-	"$SYS39": true,
-	"$SYS40": true,
-	"$SYS41": true,
-	"$SYS42": true,
-	"$SYS43": true,
-	"$SYS44": true,
-	"$SYS45": true,
-	"$SYS46": true,
-	"$SYS47": true,
-	"$SYS48": true,
-	"$SYS49": true,
-	"$SYS50": true,
-	"$SYS51": true,
-	"$SYS52": true,
-	"$SYS53": true,
-	"$SYS54": true,
-	"$SYS55": true,
-	"$SYS56": true,
-	"$SYS57": true,
-	"$SYS58": true,
-	"$SYS59": true,
-	"$SYS60": true,
-	"$SYS61": true,
-	"$SYS62": true,
-	"$SYS63": true,
-	"$PC":    true,
-	"$SP":    true,
-	"$SB":    true,
-}
-
-func LookupKeyword(keyword string) TokenType {
-	if asm_keywords[keyword] {
-		return TokenType_INSTRUCTION
-	} else if compiler_keywords[keyword] {
-		return TokenType_COMPILER
-	} else if register_keywords[keyword] {
-		return TokenType_REGISTER
-	}
-	return TokenType_IDENTIFIER
-}
-
-func NewLexer(data []rune) (*Lexer, error) {
-	sl := &Lexer{
-		data: data,
-		pos:  0,
-
-		line: 1,
-		col:  0,
-
-		state: make(map[string]interface{}),
-	}
-
-	if !sl.ReadChar() {
-		return nil, fmt.Errorf("empty input")
-	}
-	return sl, nil
+type File struct {
+	Instructions []*Instruction `parser:"@@*"`
 }
 
 func main() {
+	flags := map[string]string{}
+	for i := 1; i < len(os.Args); i++ {
+		if strings.HasPrefix(os.Args[i], "--") {
+			if i+1 < len(os.Args) && !strings.HasPrefix(os.Args[i+1], "--") && !strings.HasPrefix(os.Args[i+1], "-") {
+				flags[os.Args[i][2:]] = os.Args[i+1]
+				i++
+			} else {
+				flags[os.Args[i][2:]] = ""
+			}
+		} else if strings.HasPrefix(os.Args[i], "-") {
+			if i+1 < len(os.Args) && !strings.HasPrefix(os.Args[i+1], "--") && !strings.HasPrefix(os.Args[i+1], "-") {
+				flags[os.Args[i][1:]] = os.Args[i+1]
+				i++
+			} else {
+				flags[os.Args[i][1:]] = ""
+			}
+		} else {
+			flags["__INPUT__"] = os.Args[i]
+		}
+	}
 
+	var err error
+
+	if v, ok := flags["__INPUT__"]; !ok || v == "" {
+		log.Fatalln("No input file specified")
+	}
+
+	flags["__INPUT__"] = filepath.Clean(flags["__INPUT__"])
+	flags["__INPUT__"], err = filepath.Abs(flags["__INPUT__"])
+	if err != nil {
+		log.Fatalln("Failed to get absolute path of input file:", err)
+	}
+
+	f, err := os.Open(flags["__INPUT__"])
+	if err != nil {
+		log.Fatalln("Failed to open input file:", err)
+	}
+	defer f.Close()
+
+	parser, err := participle.Build(&File{})
+	if err != nil {
+		log.Fatalln("Failed to build parser:", err)
+	}
+
+	var file File
+	err = parser.Parse(flags["__INPUT__"], f, &file)
+	if err != nil {
+		log.Fatalln("Failed to parse input file:", err)
+	}
+
+	e := asm.NewEncoder()
+
+	var variables = map[string]uint64{}
+
+	//repr.Println(file)
+
+	var entryPoint uint64
+
+	var ops []asm.Operand
+	for _, instr := range file.Instructions {
+		ops = ops[:0]
+		instr.Name = strings.ToUpper(instr.Name)
+		//fmt.Printf("%s", instr.Name)
+		for i, operand := range instr.Operands {
+			if operand.Register != nil {
+				//fmt.Printf(" %%%s", operand.Register.Name)
+				if id, ok := lvm2.Registers[operand.Register.Name]; ok {
+					if i > 0 {
+						ops = append(ops, asm.OPREG(id))
+					} else {
+						ops = append(ops, asm.OPCONST(id))
+					}
+				} else {
+					log.Fatalln("Unknown register:", operand.Register.Name)
+				}
+			} else if operand.Int != nil {
+				if operand.Sign {
+					*operand.Int = -*operand.Int
+				}
+				//fmt.Printf(" %d", *operand.Int)
+				ops = append(ops, asm.OPCONST(uint64(*operand.Int)))
+			} else if operand.String != nil {
+				*operand.String, err = strconv.Unquote(*operand.String)
+				if err != nil {
+					log.Fatalln("Failed to unquote string:", err)
+				}
+				//fmt.Printf(" %v", []byte(*operand.String))
+
+				if instr.Name != "DATA" {
+					log.Fatalln("string operand only allowed in DATA instruction")
+				}
+			} else if operand.Variable != nil {
+				//fmt.Printf(" @%s", *operand.Variable)
+				if instr.Name == "DATA" || instr.Name == "LABEL" {
+					continue
+				}
+				if offset, ok := variables[*operand.Variable]; ok {
+					ops = append(ops, asm.OPCONST(offset))
+				} else {
+					log.Fatalln("Undefined variable:", *operand.Variable)
+				}
+			}
+		}
+		//fmt.Printf("\n")
+
+		switch instr.Name {
+		case "DATA":
+			if len(instr.Operands) != 2 {
+				log.Fatalln("DATA instruction must have exactly two operands")
+			}
+			if instr.Operands[0].Variable == nil {
+				log.Fatalln("DATA instruction's first operand must be a variable")
+			}
+			if instr.Operands[1].String == nil {
+				log.Fatalln("DATA instruction's second operand must be a string")
+			}
+
+			varName := *instr.Operands[0].Variable
+			data := []byte(*instr.Operands[1].String)
+			offset := e.Encode(asm.DATA(data))
+			variables[varName] = offset
+		case "LABEL":
+			if len(instr.Operands) != 1 {
+				log.Fatalln("LABEL instruction must have exactly one operand")
+			}
+			if instr.Operands[0].Variable == nil {
+				log.Fatalln("LABEL instruction's operand must be a variable")
+			}
+
+			varName := *instr.Operands[0].Variable
+			offset := e.Encode(asm.LABEL(varName))
+			variables[varName] = offset
+		default:
+			opcode, ok := lvm2.Instructions[instr.Name]
+			if !ok {
+				log.Fatalln("Invalid instruction:", instr.Name)
+			}
+			pc := e.Encode(asm.INST(opcode, ops...))
+			if entryPoint == 0 {
+				entryPoint = pc
+			}
+		}
+	}
+
+	//fmt.Println(e.Bytes())
+
+	vm := lvm2.VM{
+		Memory: lvm2.NewMemory(),
+		Files: map[uint64]lvm2.VMFile{
+			0: os.Stdin,
+			1: os.Stdout,
+			2: os.Stderr,
+		},
+		FileCounter: 3,
+	}
+	vm.Memory.SetProgram(e.Bytes())
+
+	// Set Initial Stack Pointer
+	vm.Registers[lvm2.REGISTER_SP] = vm.Memory.MaxAddress
+	vm.Registers[lvm2.REGISTER_SB] = vm.Memory.MaxAddress
+
+	// Set Entry Point
+	vm.SetProgramCounter(entryPoint)
+	ret, err := vm.Run()
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(int(ret))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/lemon-mint/lvm2
 
 go 1.18
+
+require github.com/alecthomas/participle/v2 v2.0.0-alpha8
+
+require github.com/alecthomas/repr v0.0.0-20220113201626-b1b626ac65ae // indirect
